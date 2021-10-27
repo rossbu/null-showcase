bec005e751c26c819cfcc540d9df3b97bcee5b6f
rossbu <ross.bu@gmail.com>
2021-10-27 15:49:47 -0400

    init the project

---
+++ src/main/java/com/showcase/nullability/NullabilityApplication.java
@@ -0,0 +1,13 @@
+package com.showcase.nullability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showcase/nullability/NullabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showcase.nullability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
